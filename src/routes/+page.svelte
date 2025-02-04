5fc3e402a4e74c42113ddbf4f14afbf270a9bccd
heloacardoso <heloa.muniz@estudante.ifms.edu.br>
2025-02-04 16:26:01 -0400

    teste 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<h1>Welcome to SvelteKit</h1>
-
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>heloa cardoso</h1> 9a0-p
+<h1>heloa cardoso</h1> 
